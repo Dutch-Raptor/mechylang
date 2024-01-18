05bc9dc083345593ca9c7217da5355b5882708fc
Pieter Huizenga <mail@pieterhuizenga.com>
2024-01-18 23:20:52 +0100

    Add fizzbuzz to doctests

--- src/docs/examples.rs
+++ src/docs/examples.rs
@@ -62,3 +62,60 @@
 /// # "#);
 /// ```
 pub struct SummingPrimes;
+
+/// # FizzBuzz
+///
+/// ## Objective
+///
+/// Create a program that prints the numbers from 1 to 100. But for multiples of three print
+/// “Fizz” instead of the number and for the multiples of five print “Buzz”. For numbers which are
+/// multiples of both three and five print “FizzBuzz”.
+///
+/// ## Solution
+///
+/// ```rust
+/// # mechylang::test_utils::test_eval_ok(r#"
+/// fn fizzbuzz(n) {
+///   let config = [
+///     [3, "Fizz"],
+///     [5, "Buzz"],
+///   ];
+///
+///   let values = [];
+///
+///   for i in 1..=n {
+///     let output = "";
+///
+///     for item in config {
+///       let divisor = item[0];
+///       let word = item[1];
+///
+///       if i % divisor == 0 {
+///         output += word;
+///       }
+///     }
+///
+///     values.push(if output == "" { i; } else { output; });
+///   }
+///
+///   return values;
+/// };
+///
+/// assert_eq(
+///     fizzbuzz(100),
+///     [
+///     1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz", 11, "Fizz", 13, 14,
+///     "FizzBuzz", 16, 17, "Fizz", 19, "Buzz", "Fizz", 22, 23, "Fizz", "Buzz",
+///     26, "Fizz", 28, 29, "FizzBuzz", 31, 32, "Fizz", 34, "Buzz", "Fizz", 37,
+///     38, "Fizz", "Buzz", 41, "Fizz", 43, 44, "FizzBuzz", 46, 47, "Fizz", 49,
+///     "Buzz", "Fizz", 52, 53, "Fizz", "Buzz", 56, "Fizz", 58, 59, "FizzBuzz",
+///     61, 62, "Fizz", 64, "Buzz", "Fizz", 67, 68, "Fizz", "Buzz", 71, "Fizz",
+///     73, 74, "FizzBuzz", 76, 77, "Fizz", 79, "Buzz", "Fizz", 82, 83, "Fizz",
+///     "Buzz", 86, "Fizz", 88, 89, "FizzBuzz", 91, 92, "Fizz", 94, "Buzz",
+///     "Fizz", 97, 98, "Fizz", "Buzz"
+///     ]
+/// );
+///
+/// "#);
+/// ```
+pub struct FizzBuzz;
